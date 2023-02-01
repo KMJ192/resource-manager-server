95da23bbf49dab4e9953ac7ef88acd193d49ed7b
MyeongJunKim <myeongjun222@naver.com>
2023-02-01 21:04:25 +0900

    nestjs webpack 설정

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async test(): Promise<number> {
+    const test = await import('@root/wasm_module/pkg').then((wasm) => {
+      const result = wasm.test();
+      return result;
+    });
+
+    return test;
+  }
 }
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nodeExternals = require('webpack-node-externals');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
+module.exports = function (options, webpack) {
+  options.resolve.alias = {
+    ...options.resolve.alias,
+    '@src': path.resolve(__dirname, 'src'),
+    '@root': path.resolve(__dirname, '.'),
+  };
+  return {
+    ...options,
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    experiments: {
+      syncWebAssembly: true,
+      asyncWebAssembly: true,
+    },
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/],
+      }),
+      new RunScriptWebpackPlugin({
+        name: options.output.filename,
+        autoRestart: false,
+      }),
+      new WasmPackPlugin({
+        crateDirectory: './wasm_module',
+      }),
+    ],
+  };
+};
