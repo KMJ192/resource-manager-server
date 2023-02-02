c697218668ba422ad73a81b6cb4399e2d072fdb5
MyeongJunKim <myeongjun222@naver.com>
2023-02-02 23:17:01 +0900

    [230202] mysql 연결

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   await app.listen(8080);
 
   if (module.hot) {
